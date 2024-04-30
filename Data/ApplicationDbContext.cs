38e4e31f78efc1665923f514b64cb8096a5ca710
Ahmed Bayoumy <Ahmed.Bayoumy@linkdev.com>
2024-04-30 22:50:55 +0300

    create project

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using AspNetIdentityExtend.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace AspNetIdentityExtend.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
